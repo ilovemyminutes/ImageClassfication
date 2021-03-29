a54dd98f0f7ceb48f15b702e1c5d4115c7c5a01f
iloveslowfood <silkstaff@naver.com>
2021-03-29 06:26:18 +0000

    add competition desc

--- model.py
+++ model.py
@@ -4,5 +4,15 @@ from torchvision import models
 
 class ResNet50(nn.Module):
     def __init__(self):
-        self.resnet
-    
+        super(ResNet50, self).__init__()
+        self.resnet = models.resnet50(pretrained=True)
+        self._freeze()
+
+    def forward(self, x):
+        output = self.resnet(x)
+
+    def _freeze(self):
+        for name, param in self.resnet.named_parameters():
+            if name not in ['fc.weight', 'fc.bias']:
+                param.requires_grad = False
+                
